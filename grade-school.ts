a79efe4e9e35ca2f816729e20e9f060f4f2a702c
gness1804 <graham.nessler@gmail.com>
2020-04-19 16:01:16 -0500

    Second test passing

--- grade-school.ts
+++ grade-school.ts
@@ -1,11 +1,12 @@
 export default class GradeSchool {
-  studentRoster: () => Map<number, string[]>;
+  studentRoster: () => Map<string, string[]>;
 
   constructor() {
     this.studentRoster = () => new Map();
   }
 
-  // addStudent(name: string, grade: number) {
-
-  // }
+  addStudent(name: string, grade: number) {
+    const map = this.studentRoster().set(grade.toString(), [name])
+    this.studentRoster = () => map;
+  }
 }
