2263ab19509fa4e12b3f88374b4386f5f062564e
gness1804 <graham.nessler@gmail.com>
2022-05-12 11:44:55 -0500

    update class to reflect updated original exercise

--- grade-school.ts
+++ grade-school.ts
@@ -1,22 +1,13 @@
-export default class GradeSchool {
-  studentRoster: () => Map<string, string[]>;
-
-  constructor() {
-    this.studentRoster = () => new Map();
+export class GradeSchool {
+  roster(): { [grade: number]: string[] } {
+    return {};
   }
 
-  addStudent(name: string, grade: number): void {
-    const gradeStr = grade.toString();
-    const map = this.studentRoster();
-    const students = map.get(gradeStr) || [];
-    map.set(gradeStr, [...students, name].sort());
-    this.studentRoster = () => map;
+  add(name: string, grade: number) {
+    throw new Error('Remove this statement and implement this function')
   }
 
-  studentsInGrade(grade: number): string[] {
-    const gradeStr = grade.toString();
-    const map = this.studentRoster();
-    const students = map.get(gradeStr) || [];
-    return students.sort();
+  grade(grade: number): string[] {
+    throw new Error('Remove this statement and implement this function')
   }
 }
