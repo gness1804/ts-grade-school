4c2cf78125b5a425968e7a5a2d4848bd882503ee
gness1804 <graham.nessler@gmail.com>
2022-05-12 13:38:43 -0500

    pass test for a student can't be in two different grades

--- grade-school.ts
+++ grade-school.ts
@@ -10,6 +10,15 @@ export class GradeSchool {
   }
 
   add(name: string, grade: number): void {
+    for (const grade in this.rosterObj) {
+      // remove the student if they are already enrolled in a grade.
+      if (Object.prototype.hasOwnProperty.call(this.rosterObj, grade)) {
+        const students = this.rosterObj[grade];
+        if (students.includes(name)) {
+          this.rosterObj[grade] = this.rosterObj[grade].filter(studentName => studentName !== name);
+        }
+      }
+    }
     const newRosterObj = {
       ...this.rosterObj,
       [grade]: [...this.rosterObj[grade] || [], name],
