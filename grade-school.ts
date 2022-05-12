b05879b9331f635937a04e08e568427f65367e8e
gness1804 <graham.nessler@gmail.com>
2022-05-12 11:52:12 -0500

    second test passing

--- grade-school.ts
+++ grade-school.ts
@@ -1,10 +1,15 @@
 export class GradeSchool {
+  rosterObj: { [grade: number]: string[] } = {};
   roster(): { [grade: number]: string[] } {
-    return {};
+    return this.rosterObj;
   }
 
-  add(name: string, grade: number) {
-    throw new Error('Remove this statement and implement this function')
+  add(name: string, grade: number): void {
+    const newRosterObj = {
+      ...this.rosterObj,
+      [grade]: [...this.rosterObj[grade] || [], name],
+    }
+    this.rosterObj = newRosterObj;
   }
 
   grade(grade: number): string[] {
